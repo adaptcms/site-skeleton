d1e7f742eb02efb88b1b834f2d670043e86e458f
Charlie Page <charliepage88@gmail.com>
2020-07-23 18:52:16 -0400

    responsive fixes

--- ui/layouts/layout.vue
+++ ui/layouts/layout.vue
@@ -2,98 +2,97 @@
   <div id="app" class="app h-screen">
     <nav class="flex items-center justify-between flex-wrap bg-blue-700 py-2 lg:sticky top-0 z-50">
       <div class="container mx-auto flex flex-wrap">
-        <div class="flex items-center flex-shrink-0 text-white mr-6 h-12">
-          <a :href="$route('home')">
+        <div class="flex items-center flex-shrink-0 text-white ml-4 lg:ml-0 lg:mr-6 h-12">
+          <inertia-link :href="$route('home')">
             <img class="flex items-center h-12" src="/img/AdaptCMSLogoPNG_2.png" alt="AdaptCMS" />
-          </a>
+          </inertia-link>
         </div>
-        <div class="block my-auto ml-auto lg:hidden">
-          <button id="toggle-menu" class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
-            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
-            </svg>
+        <div class="block my-auto ml-auto lg:hidden pr-4">
+          <button
+            class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white"
+            @click.prevent="showMobileMenu = !showMobileMenu"
+          >
+            <i class="fas fa-bars fa-lg" />
           </button>
         </div>
 
-        <div id="menu" class="w-full block flex-grow flex-wrap lg:flex lg:items-center lg:text-right lg:w-auto hidden">
-          <ul class="text-sm lg:flex-grow lg:pr-5">
+        <div
+          class="w-full block flex-grow flex-wrap lg:flex lg:items-center lg:text-right lg:w-auto ml-4 lg:ml-0"
+          :class="{ 'hidden': !showMobileMenu }"
+        >
+          <ul class="text-base lg:flex-grow">
             <li class="block mt-6 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-6">
-              <a :href="$route('home')" class="font-semibold lg:font-normal">
+              <inertia-link
+                :href="$route('home')" class="hover:text-indigo-300"
+                :class="{ 'font-bold text-indigo-300': routeName === 'home' }"
+              >
                 Home
-              </a>
+              </inertia-link>
             </li>
 
             <li
               v-for="module in modules"
               :key="`module-${module.slug}`"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-6"
+              class="block mt-2 lg:mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-6"
             >
-              <a :href="$route('modules.custom.index', { module: module.route_key })">
+              <inertia-link
+                :href="$route('modules.custom.index', { module: module.route_key })"
+                class="hover:text-indigo-300"
+                :class="{ 'font-bold text-indigo-300': routeUrl === $route('modules.custom.index', { module: module.route_key }) }"
+              >
                 {{ module.name }}
-              </a>
+              </inertia-link>
             </li>
 
             <li
               v-for="page in customPages"
               :key="`page-${page.slug}`"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-6"
+              class="block mt-2 lg:mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-6"
             >
-              <a :href="$route(`pages.show.${page.slug}`)" class="font-semibold lg:font-normal">
+              <inertia-link
+                :href="$route(`pages.show.${page.slug}`)"
+                class="hover:text-indigo-300"
+                :class="{ 'font-bold text-indigo-300': routeName === `pages.show.${page.slug}` }"
+              >
                 {{ page.name }}
-              </a>
-            </li>
-
-            <li
-              v-if="!user"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-            >
-              <a :href="$route('login')" class="font-semibold lg:font-normal">
-                Login
-              </a>
+              </inertia-link>
             </li>
 
             <li
               v-if="user"
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+              class="block mt-2 lg:mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-6"
             >
-              <a :href="$route('logout')" class="font-semibold lg:font-normal">
+              <inertia-link :href="$route('logout')" class="hover:text-indigo-300">
                 Logout
-              </a>
+              </inertia-link>
             </li>
           </ul>
 
           <div v-if="user">
-            <a
+            <inertia-link
               :href="$route('auth.dashboard')"
-              class="inline-block text-sm px-4 py-2 leading-none border rounded-full text-black bg-white border-white hover:border-transparent hover:text-white hover:bg-black mt-4 lg:mt-0"
+              class="inline-block px-4 py-2 font-bold leading-none border rounded-full text-black bg-white border-white hover:border-transparent hover:text-white hover:bg-indigo-400 mt-4 lg:mt-0"
             >
               Dashboard
-            </a>
+            </inertia-link>
           </div>
 
           <div v-else>
-            <a
-              :href="$route('register')"
-              class="inline-block text-sm px-4 py-2 leading-none border rounded-full text-black bg-white border-white hover:border-transparent hover:text-white hover:bg-black mt-4 lg:mt-0"
+            <inertia-link
+              :href="$route('login')"
+              class="inline-block px-4 py-2 font-bold leading-none border rounded-full text-black bg-white border-white hover:border-transparent hover:text-white hover:bg-indigo-400 mt-4 lg:mt-0"
             >
-              Register
-            </a>
+              Login
+            </inertia-link>
           </div>
         </div>
       </div>
     </nav>
 
-    <div class="content h-auto py-6 px-64">
+    <div class="content h-auto py-6 container mx-auto px-4 md:px-0">
       <slot />
 
-      <template v-if="flashMessage">
-        <div
-          class="w-64 fixed right-0 top-0 p-4 text-sm shadow text-white bg-green-500 border border-green-700 text-green-100 rounded mr-4 mt-4 z-50 text-sm text-center"
-        >
-          {{ flashMessage }}
-        </div>
-      </template>
+      <FlashMessage :message="flashMessage" />
     </div>
 
     <div class="top-divider w-full"></div>
@@ -101,11 +100,11 @@
     <footer class="bg-black pt-8 pb-12 w-full bottom-0">
       <div class="container mx-auto">
         <div class="w-full h-auto text-center pt-5">
-          <p class="mb-5 text-xs text-gray-100">
+          <!-- <p class="mb-5 text-xs text-gray-100">
             <a href="#">Privacy Policy</a> | <a href="#">Terms and Conditions</a>
-          </p>
+          </p> -->
           <p class="text-xs text-gray-100">
-            &copy; Copyright 2020 <a class="underline" href="https://www.adaptcms.com" target="_blank" rel="noopener">AdaptCMS</a>.
+            &copy; Copyright 2020 <a href="https://www.adaptcms.com" class="font-bold" target="_blank" rel="noopener">AdaptCMS</a>.
             All Rights Reserved.
           </p>
         </div>
@@ -116,20 +115,28 @@
 
 <script>
 import { get } from 'lodash'
+import FlashMessage from '@/Adaptcms/Base/ui/components/FlashMessage'
+import FlashMessageMixin from '@/Adaptcms/Base/ui/mixins/FlashMessageMixin'
 
 export default {
   props: {
     title: String
   },
 
+  mixins: [
+    FlashMessageMixin
+  ],
+
+  components: {
+    FlashMessage
+  },
+
   watch: {
-    flashMessage (newVal, oldVal) {
-      if (newVal && (newVal !== oldVal)) {
-        setTimeout(() => {
-          this.$set(this.$page, 'flash', {
-            message: null
-          })
-        }, 2500)
+    routeUrl (newVal, oldVal) {
+      if (newVal !== oldVal) {
+        if (this.showMobileMenu) {
+          this.showMobileMenu = false
+        }
       }
     }
   },
@@ -139,16 +146,20 @@ export default {
       return get(this.$page, 'auth.user', null)
     },
 
-    flashMessage () {
-      return get(this.$page, 'flash.message', null)
-    },
-
     pages () {
       return get(this.$page, 'pages', null)
     },
 
     customPages () {
       return this.pages.filter(page => page.slug !== 'home')
+    },
+
+    routeName () {
+      return get(this.$page, 'route.name', null)
+    },
+
+    routeUrl () {
+      return get(this.$page, 'route.url', null)
     }
   },
 
@@ -159,7 +170,8 @@ export default {
         enabled: false,
         type: 'success',
         message: null
-      }
+      },
+      showMobileMenu: false
     }
   },
 
